c461384952a8f79f2a8712dcddcdd0bd264af8c4
AnDyomin <dyominandrey1992@gmail.com>
2024-10-06 19:41:12 +0300

    Структурирование кода 2

--- sandbox/src/main/java/ru/test/geometry/Geometry.java
+++ sandbox/src/main/java/ru/test/geometry/Geometry.java
@@ -6,8 +6,8 @@ import ru.test.geometry.figures.Square;
 public class Geometry {
     public static void main(String[] args) {
 
-        Square.printSquareArea(7.);
-        Square.printSquareArea(2.);
+        Square.printSquareArea(new Square(7.));
+        Square.printSquareArea(new Square(2.));
 
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(2.0, 4.0);
